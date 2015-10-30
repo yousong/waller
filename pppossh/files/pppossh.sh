6f567bd8c82a8dd53fb3a9db52f1f4d48951098f
Yousong Zhou <yszhou4tech@gmail.com>
2015-10-30 16:03:23 +0800

    pppossh: use exec with pty option to eliminate a level of child process

--- pppossh/files/pppossh.sh
+++ pppossh/files/pppossh.sh
@@ -60,7 +60,7 @@ proto_pppossh_setup() {
 	opts="$opts ${port:+-p $port}"
 	opts="$opts ${ssh_options}"
 	opts="$opts $sshuser@$server"
-	pty="env 'HOME=$home' "$SSH" $opts pppd nodetach notty noauth"
+	pty="exec env 'HOME=$home' "$SSH" $opts pppd nodetach notty noauth"
 
 	ppp_generic_setup "$config" noauth pty "$pty" "$ipaddr:$peeraddr"
 }
